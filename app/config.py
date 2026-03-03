7b83ba58b5f948e9f3e9411ebe27f1eff591def1
AnumMehar <92399260+AnumMehar@users.noreply.github.com>
2026-03-03 22:11:05 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,26 +1,3 @@
-# import os
-# from dotenv import load_dotenv
-# from pydantic_settings import BaseSettings
-#
-# load_dotenv()
-#
-# DATABASE_URL = os.getenv("DATABASE_URL")
-#
-# if not DATABASE_URL:
-#     raise RuntimeError("DATABASE_URL is not set")
-#
-# APP_ENV = os.getenv("APP_ENV", "development")
-#
-#
-#
-# class Settings(BaseSettings):
-#     DATABASE_URL: str
-#     # DIRECT_URL: str
-#     CLOUD_DATABASE_URL: str
-#     JWT_ALGORITHM: str = "RS256"
-#     ACCESS_TOKEN_EXPIRE_DAYS: int = 3
-#
-# settings = Settings()
 
 import os
 from dotenv import load_dotenv
@@ -39,5 +16,6 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
